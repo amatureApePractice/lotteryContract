f232cad8512756423914871faeab3117cc135b55
drdarian <dariantaban@gmail.com>
2021-07-02 20:34:31 -0700

    Fixed deploymnet issues

--- compile.js
+++ compile.js
@@ -5,5 +5,5 @@ const solc = require('solc');
 const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
 const source = fs.readFileSync(lotteryPath, 'utf8');
 
-console.log(solc.compile(source, 1))
+// console.log(solc.compile(source, 1).contracts[':Lottery']);
 module.exports = solc.compile(source, 1).contracts[':Lottery'];
--- deploy.js
+++ deploy.js
@@ -2,7 +2,11 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
-const provider = new Web3(provider);
+const provider = new HDWalletProvider(
+  'card lobster caution fetch cabbage build move legend monster coach face assault',
+  'https://rinkeby.infura.io/v3/3e62532167e1432e96f4dc3b31ba8122'
+);
+const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
@@ -13,6 +17,7 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
-  console.log('Contract deployed to ', result.option.address);
+  console.log(interface);
+  console.log('Contract deployed to', result.options.address);
 };
 deploy();
